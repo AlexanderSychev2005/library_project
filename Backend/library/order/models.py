0e616e7da92b5b81b2f72ee06939f0a2e8627930
Alex <alexandersychev2005@gmail.com>
2025-03-13 11:18:06 +0200

    Updates templates and added blank option to returned_at field in StudentOrder model

--- Backend/library/order/models.py
+++ Backend/library/order/models.py
@@ -11,7 +11,7 @@ class StudentOrder(models.Model):
     student = models.ForeignKey('student.Student', on_delete=models.CASCADE)
     book = models.ForeignKey('book.Book', on_delete=models.CASCADE)
     created_at = models.DateField()
-    returned_at = models.DateField(null=True)
+    returned_at = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=25, choices=STATUS_CHOICES)
 
     def __str__(self):
